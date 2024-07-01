8a074a2b2c6cb3c1fc1b10d6db3654c203c73915
Humiho <pc@Huyyys-MacBook-Pro.local>
2024-07-01 10:00:24 +0700

    Initial commit

---
+++ library.h
@@ -0,0 +1,32 @@
+//
+// Created by Humiho on 1/7/24.
+//
+
+#ifndef LEETCODE_LIBRARY_H
+#define LEETCODE_LIBRARY_H
+
+#endif //LEETCODE_LIBRARY_H
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "library.h"
+
+typedef long long ll;
+
+class Implement {
+public:
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
